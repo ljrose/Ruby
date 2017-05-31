80c46f6234066030778cf63ea465472ead9c4c64
LjRose <ljr@ljroseonline.com>
2017-05-31 16:28:38 -0700

    Final ex20.rb

--- ex20.rb
+++ ex20.rb
@@ -4,12 +4,12 @@ input_file = ARGV.first
 def print_all(f)
 	puts f.read
 end
-# Create function that reads file entered L.1 and prints to scrn
+# Create function that reads what is stored in 1st argv
 
 def rewind(f)
 	f.seek(0)
 end
-# Reset the file start point to the beginning of file
+# Creates func that resets file start point to the beginning of file
 
 def print_a_line(line_count, f)
 	puts "#{line_count}, #{f.gets.chomp}"
@@ -17,27 +17,28 @@ end
 # creates function counts lines in the file and prints # to scrn
 
 current_file = open(input_file)
-#  Open the file from L1
+#  Open ARGV.first and store in variable 'current file'
 
 puts "First let's print the whole file:\n"
 
-
 print_all(current_file)
-# Print contents of file mentioned to scrn
+# Print contents of current_file to scrn
 
 puts "Now let's rewind, kind of like a tape."
 
 rewind(current_file)
-# Rewind to start of file
+# Rewind current_file to beginning
 
 puts "Let's print 3 lines:"
 
 current_line = 1
 print_a_line(current_line, current_file)
-# 
+# Assigns line 1 of current_file to variable 
+
+current_line = current_line +=
+print_a_line(current_line, current_file)
+# Increments current line in current_file by 1 and prints
 
 current_line = current_line + 1
 print_a_line(current_line, current_file)
-
-current_line = current_line + 1
-print_a_line(current_line, current_file)
+# Increments new value stored of current_line by 1 and prints
