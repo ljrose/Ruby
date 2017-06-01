03776fb253ee24a5fef9b7326b88e2572321e6b2
LjRose <ljr@ljroseonline.com>
2017-06-01 16:06:02 -0700

    Playing with omitting 'return' command

---
+++ ex21_a.rb
@@ -0,0 +1,25 @@
+# BEGIN creation of the math functions to be used
+
+def add(a, b)
+	return a + b
+end
+
+# END math functions
+
+
+# BEGIN build the formulas using the above functions and create variables to hold values returned
+
+
+puts "Let's do some math with just functions!"
+
+age = add(30, 5)
+
+# END formulas and variable assignments
+
+# BEGIN actual calculations and print results to screen
+
+puts "Age: #{age}"
+
+# END calculations and display of results
+
+
